b5f0dc7a3c1a145744225ae79cef31cb00820c08
NGUYEN THANH HAI <thanhhain93@gmail.com>
2020-09-27 02:27:51 +0800

    initial commit

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 """
 Name: NGUYEN THANH HAI
 Date: 27/09/2020
-Brief Project Description:
+Brief Project Description: An application that tracks places for user visit
 GitHub URL:https://github.com/JCUS-CP1404/assignment-2-travel-tracker-thanhhai93
 """
 # Create your main program in this file, using the TravelTrackerApp class
 
 from kivy.app import App
-
-
 class TravelTrackerApp(App):
     """..."""
     pass
