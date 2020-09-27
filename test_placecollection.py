25439280516d6215868eccc4592cbf17597bd711
NGUYEN THANH HAI <thanhhain93@gmail.com>
2020-09-27 21:42:08 +0800

    initial commit

--- test_placecollection.py
+++ test_placecollection.py
@@ -1,67 +1,50 @@
-"""(Incomplete) Tests for PlaceCollection class."""
 from placecollection import PlaceCollection
 from place import Place
-
+import csv
 
 def run_tests():
-    """Test PlaceCollection class."""
+    """Test Collection class."""
 
-    # Test empty PlaceCollection (defaults)
-    print("Test empty PlaceCollection:")
+    # Test empty Collection (defaults)
+    print("Test empty Collection:")
     place_collection = PlaceCollection()
     print(place_collection)
-    assert not place_collection.places  # an empty list is considered False
+    assert not place_collection.place  # an empty list is considered False
 
     # Test loading places
     print("Test loading places:")
     place_collection.load_places('places.csv')
     print(place_collection)
-    assert place_collection.places  # assuming CSV file is non-empty, non-empty list is considered True
+    assert place_collection.place_list  # assuming CSV file is non-empty, non-empty list is considered True
 
-    # Test adding a new Place with values
-    print("Test adding new place:")
-    place_collection.add_place(Place("Smithfield", "Australia", 5, False))
+    # Test adding a new lace with values
+    print("Test adding new place :")
+    place_collection.add_place(Place ("Smithfield", "Australia", 5, False))
     print(place_collection)
 
     # Test sorting places
-    print("Test sorting - priority:")
-    place_collection.sort("priority")
-    print(place_collection)
-    # TODO: Add more sorting tests
+    print("Test sorting - Priority:")
+    sorted_list = place_collection.sort_place("priority ")
+    print(place_collection.format_data(sorted_list))
 
-    # TODO: Test saving places (check CSV file manually to see results)
+    # Test sorting country
+    print("Test sorting - country:")
+    sorted_list_2 = place_collection.sort_place("country", descending=True)
+    print(place_collection.format_data(sorted_list_2))
+    # test sorting name
+    print("Test sorting - name:")
+    sorted_list_3 = place_collection.sort_place("name", descending=False)
+    print(place_collection.format_data(sorted_list_3))
+ #  Test saving places (check CSV file manually to see results)
+    print("Test saving places:")
+    place_collection.save_changes("places.csv")
 
-    # TODO: Add more tests, as appropriate, for each method
-
-    # TODO: Add more sorting tests
-    """Sorting tests"""
-    print("Test sorting-Alphabetically:")
-    place_collection.sort_Alphabetically(place_collection.places)
-    print(place_collection)
-
-    print("Test sorting by vivsited status")
-    place_collection.sort_visited(place_collection.places)
-    print(place_collection)
-
-    print("Test sorting by name")
-    place_collection.sort_name(place_collection.places)
-    print(place_collection)
-
-    print("Test sorting by country")
-    place_collection.sort_country(place_collection.places)
-    print(place_collection)
-
-    print("Test sorting by unvisited status")
-    place_collection.sort_unvisited(place_collection.places)
-    print(place_collection)
-    # TODO: Test saving places (check CSV file manually to see results)
-    """Save file from places list that is stored in the csv file"""
-    print("test saving")
-    place_collection.save_file('places.csv')
-    """New edited file is now saved and the csv is updated!"""
-    assert place_collection.places
-    # TODO: Add more tests, as appropriate, for each method
-    print(place_collection.get_number_unvisited_places())
+    # Add more tests, as appropriate, for each method
+    print("Test count learned places:")
+    print(place_collection.count_learned())
+    # Test count unlearned places
+    print("Test count unlearned places:")
+    print(place_collection.count_unlearned())
 
 
 run_tests()
